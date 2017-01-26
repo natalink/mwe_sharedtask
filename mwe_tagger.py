204af3e50a9e3d8a6ac64251f92968218b6d5fd6
kljueva <natalink@seznam.cz>
2017-01-26 17:11:34 +0100

    some implementation of saver

--- mwe_tagger.py
+++ mwe_tagger.py
@@ -14,7 +14,7 @@ import tensorflow.contrib.metrics as tf_metrics
 import mwe_dataset
 
 class Network:
-    def __init__(self, rnn_cell, rnn_cell_dim, method, data_train, logdir, expname, threads=1, seed=42):
+    def __init__(self, rnn_cell, rnn_cell_dim, method, data_train, logdir, expname, threads, restore_path ,seed=42):
         n_words = len(data_train.factors[data_train.FORMS]['words'])
         n_tags = len(data_train.factors[data_train.TAGS]['words'])
         n_lemmas = len(data_train.factors[data_train.LEMMAS]['words'])
@@ -25,9 +25,12 @@ class Network:
         graph.seed = seed
         self.session = tf.Session(graph = graph, config=tf.ConfigProto(inter_op_parallelism_threads=threads,
                                                                        intra_op_parallelism_threads=threads))
-
+        if restore_path is not None:
+            self.saver.restore(self.session, restore_path)
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
         self.summary_writer = tf.train.SummaryWriter("{}/{}-{}".format(logdir, timestamp, expname), flush_secs=10)
+
+
 
         # Construct the graph
         with self.session.graph.as_default():
@@ -46,12 +49,12 @@ class Network:
             self.mwe = tf.placeholder(tf.int32, [None, None])
             embeddings_forms = tf.get_variable("word_embedding_matrix", [n_words,rnn_cell_dim], dtype = tf.float32)
             embeddings_lemmas = tf.get_variable("lemma_embedding_matrix", [n_lemmas,rnn_cell_dim], dtype = tf.float32)
-#            embeddings_tags = tf.get_variable("tag_embedding_matrix", [n_tags,rnn_cell_dim], dtype = tf.float32)
+            embeddings_tags = tf.get_variable("tag_embedding_matrix", [n_tags,rnn_cell_dim], dtype = tf.float32)
             embedding_in_forms = tf.nn.embedding_lookup(embeddings_forms, self.forms)
             embedding_in_lemmas = tf.nn.embedding_lookup(embeddings_lemmas, self.lemmas)
-#            embedding_in_tags =  tf.nn.embedding_lookup(embeddings_tags, self.tags)
+            embedding_in_tags =  tf.nn.embedding_lookup(embeddings_tags, self.tags)
            #TODO concatenata tf.cvoncat emb_form emb_lemma emb_tags (zkusit udelat one-hot na tag - nebo zapomenout na to)
-            embedding_in = tf.concat(2, [embedding_in_forms, embedding_in_lemmas]) #, embedding_in_tags]) #mail Milanovi - je to tak???
+            embedding_in = tf.concat(2, [embedding_in_forms, embedding_in_lemmas, embedding_in_tags]) #mail Milanovi - je to tak???
             bi_out, bi_out_states = tf.nn.bidirectional_dynamic_rnn(rnn_cell, rnn_cell, embedding_in, self.sentence_lens, dtype=tf.float32) # concatenate embedding
             layer1 = tf.concat(2, bi_out)
             layer = tf_layers.fully_connected(layer1, n_mwe,activation_fn=None, scope="output_layer" )
@@ -71,8 +74,12 @@ class Network:
             self.summary = tf.merge_summary([tf.scalar_summary(self.dataset_name+"/loss", loss),
                                              tf.scalar_summary(self.dataset_name+"/accuracy", self.accuracy)])
 
+            self.saver = tf.train.Saver(max_to_keep=None)
             # Initialize variables
             self.session.run(tf.initialize_all_variables())
+
+    def save(self, saved_path):
+        self.saver.save(self.session, saved_path)
 
     @property
     def training_step(self):
@@ -101,6 +108,12 @@ class Network:
                                  self.lemmas: lemmas,
                                  self.tags: tags},
 )
+    def predict_saved(self, sentence_lens, forms, lemmas, tags):
+        return self.session.run(self.predictions,
+                                {self.sentence_lens: sentence_lens, self.forms: forms,
+                                 self.lemmas: lemmas,
+                                 self.tags: tags},
+)
 
 if __name__ == "__main__":
     # Fix random seed
@@ -119,6 +132,7 @@ if __name__ == "__main__":
     parser.add_argument("--rnn_cell", default="GRU", type=str, help="RNN cell type.")
     parser.add_argument("--rnn_cell_dim", default=100, type=int, help="RNN cell dimension.")
     parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    parser.add_argument("--restore", default=None, type=str, help="Restore session with a model")
     args = parser.parse_args()
 
     # Load the data
@@ -131,7 +145,7 @@ if __name__ == "__main__":
     print("Constructing the network.", file=sys.stderr)
     expname = "tagger-{}{}-m{}-bs{}-epochs{}".format(args.rnn_cell, args.rnn_cell_dim, args.method, args.batch_size, args.epochs)
     network = Network(rnn_cell=args.rnn_cell, rnn_cell_dim=args.rnn_cell_dim, method=args.method,
-                      data_train = data_train, logdir=args.logdir, expname=expname, threads=args.threads)
+                      data_train = data_train, logdir=args.logdir, expname=expname, threads=args.threads, restore_path=args.restore)
 
     # Train
     best_dev_accuracy = 0
@@ -141,11 +155,17 @@ if __name__ == "__main__":
         print("Training epoch {}".format(epoch + 1), file=sys.stderr)
         while not data_train.epoch_finished():
             sentence_lens, word_ids = data_train.next_batch(args.batch_size)
+            saved_model = "saved_model_{}".format(expname)
+            if args.restore is not None:
+                continue;
+            else:
+                network.predict_saved()
+
             network.train(sentence_lens, word_ids[data_train.FORMS], word_ids[data_train.LEMMAS], word_ids[data_train.TAGS], word_ids[data_train.MWE]) # lemma_ids deprel_ids
             # To use character-level embeddings, pass including_charseqs=True to next_batch
             # and instead of word_ids[data_train.FORMS] use charseq_ids[data_train.FORMS],
             # charseqs[data_train.FORMS] and charseq_lens[data_train.FORMS]
-
+            network.save(saved_model)
         dev_sentence_lens, dev_word_ids = data_dev.whole_data_as_batch()
         dev_accuracy = network.evaluate(dev_sentence_lens, dev_word_ids[data_dev.FORMS],dev_word_ids[data_dev.LEMMAS], dev_word_ids[data_dev.TAGS], dev_word_ids[data_dev.MWE])
         print("Development accuracy after epoch {} is {:.2f}.".format(epoch + 1, 100. * dev_accuracy), file=sys.stderr)
@@ -154,15 +174,20 @@ if __name__ == "__main__":
             best_dev_accuracy = dev_accuracy
             test_sentence_lens, test_word_ids = data_test.whole_data_as_batch()
             test_predictions = network.predict(test_sentence_lens, test_word_ids[data_test.FORMS], test_word_ids[data_test.LEMMAS], test_word_ids[data_test.TAGS] ) #ALSO< lemmas and tags
-
+            print ("test_predictions: ", test_predictions)
     # Print test predictions
     test_forms = data_test.factors[data_test.FORMS]['strings'] # We use strings instead of words, because words can be <unk>
     test_lemmas = data_test.factors[data_test.LEMMAS]['words']
     test_tags = data_test.factors[data_test.TAGS]['words']
     test_mwe = data_test.factors[data_test.MWE]['words']
 
-    f1=open('testfile', 'w+',  encoding="utf-8")
+    import time
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    print (timestr)
+    testfilename = '{}_{}_{}'.format(args.data_train,args.epochs,timestr)
+    f1=open(testfilename, 'w+',  encoding="utf-8")
     for i in range(len(data_test.sentence_lens)):
         for j in range(data_test.sentence_lens[i]):
-            print("{}\t_\t{}".format(test_forms[i][j], test_mwe[test_predictions[i, j]]))#, file=f1)
-        print((), file=f1)
+            print("{}\t_\t{}".format(test_forms[i][j], test_mwe[test_predictions[i, j]]), file=f1)
+            #print("{}\t_\t{}".format(test_forms[i][j], test_mwe[test_predictions[i, j]]))
+        print("", file=f1)
